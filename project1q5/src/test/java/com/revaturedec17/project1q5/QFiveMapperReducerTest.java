cad3f043cdc9ca08f223db6832c62369ff62a1ef
Ben <bkeeper94@gmail.com>
2019-01-20 04:13:16 -0500

    Added Hadoop Results for Custom Question
    Solution to my custom question was run
    in hadoop.
    The results folder has been added
    and the post-processing excel document
    now has a spreadsheet for my custom
    question's solution.

--- project1q5/src/test/java/com/revaturedec17/project1q5/QFiveMapperReducerTest.java
+++ project1q5/src/test/java/com/revaturedec17/project1q5/QFiveMapperReducerTest.java
@@ -26,8 +26,8 @@ public class QFiveMapperReducerTest {
 	}
 
 	/**
-	 * These 3 MRUnit test methods will test map-reduce on one of the rows of the
-	 * csv file that will contribute to the overall output of this hadoop job.
+	 * These 3 MRUnit test methods will test map-reduce on a few of the rows in the
+	 * csv file that may contribute to the overall output of this hadoop job.
 	 */
 
 	@Test
@@ -44,16 +44,26 @@ public class QFiveMapperReducerTest {
 	}
 
 	@Test
+	public void testMapperAgain() {
+		String record1 = "\"Europe & Central Asia\",\"ECS\",\"Start-up procedures to register a business, female (number)\",\"IC.REG.PROC.FE\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"9.47619047619048\",\"8.88372093023256\",\"8.88636363636364\",\"8.45652173913044\",\"8.10869565217391\",\"7.27659574468085\",\"6.79166666666667\",\"6.54166666666667\",\"6.22916666666667\",\"6.14285714285714\",\"5.63469387755102\",\"5.37551020408163\",\"5.08979591836735\",\"4.9734693877551\",";
+		String expectedKey1 = "Europe & Central Asia";
+		String expectedValue1 = "9.47619047619048,IC.REG.PROC.FE";
+		mapDriver.withInput(new LongWritable(), new Text(record1));
+		mapDriver.withOutput(new Text(expectedKey1), new Text(expectedValue1));
+		mapDriver.runTest();
+	}
+
+	@Test
 	public void testReducer() {
-		String inputVal1 = "6,IC.REG.PROC.FE";
-		String inputVal2 = "6,IC.REG.PROC.MA";
-		String inputVal3 = "6,IC.REG.DURS.FE";
-		String inputVal4 = "6,IC.REG.DURS.MA";
+		String inputVal11 = "6,IC.REG.PROC.FE";
+		String inputVal12 = "6,IC.REG.PROC.MA";
+		String inputVal13 = "6,IC.REG.DURS.FE";
+		String inputVal14 = "6,IC.REG.DURS.MA";
 		List<Text> values1 = new ArrayList<Text>();
-		values1.add(new Text(inputVal1));
-		values1.add(new Text(inputVal2));
-		values1.add(new Text(inputVal3));
-		values1.add(new Text(inputVal4));
+		values1.add(new Text(inputVal11));
+		values1.add(new Text(inputVal12));
+		values1.add(new Text(inputVal13));
+		values1.add(new Text(inputVal14));
 		String key1 = "United States";
 		String expectedValue1 = "6-days to start business (female):6-procedures to register business (female):"
 				+ "6-days to start business (male):6-procedures to register business (male)";
@@ -63,23 +73,42 @@ public class QFiveMapperReducerTest {
 	}
 
 	@Test
+	public void testReducerAgain() {
+		String inputVal21 = "9.47619047619048,IC.REG.PROC.FE";
+		String inputVal22 = "9.47619047619048,IC.REG.PROC.MA";
+		String inputVal23 = "42.7738095238095,IC.REG.DURS.FE";
+		String inputVal24 = "42.7738095238095,IC.REG.DURS.MA";
+		List<Text> values2 = new ArrayList<Text>();
+		values2.add(new Text(inputVal21));
+		values2.add(new Text(inputVal22));
+		values2.add(new Text(inputVal23));
+		values2.add(new Text(inputVal24));
+		String key2 = "Europe and Central Asia";
+		String expectedValue1 = "42-days to start business (female):9-procedures to register business (female):"
+				+ "42-days to start business (male):9-procedures to register business (male)";
+		reduceDriver.withInput(new Text(key2), values2);
+		reduceDriver.withOutput(new Text(key2), new Text(expectedValue1));
+		reduceDriver.runTest();
+	}
+
+	@Test
 	public void testMapReduce() {
-		String record1 = "\"United States\",\"USA\",\"Time required to start up a business, female (days)\","
+		String record11 = "\"United States\",\"USA\",\"Time required to start up a business, female (days)\","
 				+ "\"IC.REG.DURS.FE\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
 				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
 				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"6\",\"6\",\"6\",\"6\",\"6\",\"5\","
 				+ "\"5\",\"5\",\"5\",\"5\",\"6.2\",\"5.6\",\"5.6\",\"5.6\",";
-		String record2 = "\"United States\",\"USA\",\"Start-up procedures to register a business, female (number)\","
+		String record12 = "\"United States\",\"USA\",\"Start-up procedures to register a business, female (number)\","
 				+ "\"IC.REG.PROC.FE\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
 				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
 				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"6\",\"6\",\"6\",\"6\",\"6\",\"6\",\"6\",\"6\",\"6\",\"6\",\"6\","
 				+ "\"6\",\"6\",\"6\",";
-		String record3 = "\"United States\",\"USA\",\"Start-up procedures to register a business, male (number)\","
+		String record13 = "\"United States\",\"USA\",\"Start-up procedures to register a business, male (number)\","
 				+ "\"IC.REG.PROC.MA\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
 				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
 				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"6\",\"6\",\"6\",\"6\",\"6\",\"6\",\"6\",\"6\",\"6\","
 				+ "\"6\",\"6\",\"6\",\"6\",\"6\",";
-		String record4 = "\"United States\",\"USA\",\"Time required to start up a business, male (days)\","
+		String record14 = "\"United States\",\"USA\",\"Time required to start up a business, male (days)\","
 				+ "\"IC.REG.DURS.MA\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
 				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
 				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"6\",\"6\",\"6\",\"6\",\"6\",\"5\","
@@ -87,11 +116,48 @@ public class QFiveMapperReducerTest {
 		String expectedKeyResult1 = "United States";
 		String expectedValueResult1 = "6-days to start business (female):6-procedures to register business (female):"
 				+ "6-days to start business (male):6-procedures to register business (male)";
-		mapReduceDriver.withInput(new LongWritable(), new Text(record1));
-		mapReduceDriver.withInput(new LongWritable(), new Text(record2));
-		mapReduceDriver.withInput(new LongWritable(), new Text(record3));
-		mapReduceDriver.withInput(new LongWritable(), new Text(record4));
+		mapReduceDriver.withInput(new LongWritable(), new Text(record11));
+		mapReduceDriver.withInput(new LongWritable(), new Text(record12));
+		mapReduceDriver.withInput(new LongWritable(), new Text(record13));
+		mapReduceDriver.withInput(new LongWritable(), new Text(record14));
 		mapReduceDriver.withOutput(new Text(expectedKeyResult1), new Text(expectedValueResult1));
+		mapReduceDriver.runTest();
+	}
+
+	@Test
+	public void testMapReduceAgain() {
+		String record21 = "\"Europe & Central Asia\",\"ECS\",\"Start-up procedures to register a business, female (number)\","
+				+ "\"IC.REG.PROC.FE\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
+				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
+				+ "\"\",\"\",\"\",\"\",\"\",\"9.47619047619048\",\"8.88372093023256\",\"8.88636363636364\",\"8.45652173913044\","
+				+ "\"8.10869565217391\",\"7.27659574468085\",\"6.79166666666667\",\"6.54166666666667\",\"6.22916666666667\","
+				+ "\"6.14285714285714\",\"5.63469387755102\",\"5.37551020408163\",\"5.08979591836735\",\"4.9734693877551\",";
+		String record22 = "\"Europe & Central Asia\",\"ECS\",\"Start-up procedures to register a business, male (number)\","
+				+ "\"IC.REG.PROC.MA\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
+				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
+				+ "\"\",\"\",\"\",\"\",\"\",\"9.47619047619048\",\"8.88372093023256\",\"8.88636363636364\",\"8.45652173913044\","
+				+ "\"8.10869565217391\",\"7.27659574468085\",\"6.79166666666667\",\"6.54166666666667\",\"6.22916666666667\","
+				+ "\"6.14285714285714\",\"5.63469387755102\",\"5.37551020408163\",\"5.08979591836735\",\"4.9734693877551\",";
+		String record23 = "\"Europe & Central Asia\",\"ECS\",\"Time required to start up a business, female (days)\","
+				+ "\"IC.REG.DURS.FE\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
+				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
+				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"42.7738095238095\",\"37.5\",\"32.6477272727273\",\"27.8586956521739\","
+				+ "\"24.054347826087\",\"20.2446808510638\",\"17.96875\",\"16.6145833333333\",\"15.1875\",\"14.8673469387755\","
+				+ "\"13.6918367346939\",\"12.2387755102041\",\"10.1632653061224\",\"9.73061224489796\",";
+		String record24 = "\"Europe & Central Asia\",\"ECS\",\"Time required to start up a business, male (days)\","
+				+ "\"IC.REG.DURS.MA\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
+				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\","
+				+ "\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"42.7738095238095\",\"37.5\",\"32.6477272727273\",\"27.8586956521739\","
+				+ "\"24.054347826087\",\"20.2446808510638\",\"17.96875\",\"16.6145833333333\",\"15.1875\",\"14.8673469387755\","
+				+ "\"13.6918367346939\",\"12.2387755102041\",\"10.1632653061224\",\"9.73061224489796\",";
+		String expectedKeyResult2 = "Europe & Central Asia";
+		String expectedValueResult2 = "42-days to start business (female):9-procedures to register business (female):"
+				+ "42-days to start business (male):9-procedures to register business (male)";
+		mapReduceDriver.withInput(new LongWritable(), new Text(record21));
+		mapReduceDriver.withInput(new LongWritable(), new Text(record22));
+		mapReduceDriver.withInput(new LongWritable(), new Text(record23));
+		mapReduceDriver.withInput(new LongWritable(), new Text(record24));
+		mapReduceDriver.withOutput(new Text(expectedKeyResult2), new Text(expectedValueResult2));
 		mapReduceDriver.runTest();
 	}
 }
